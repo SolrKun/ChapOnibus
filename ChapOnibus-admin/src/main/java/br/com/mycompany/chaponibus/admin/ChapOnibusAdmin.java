3ffc6c5fd1da3b3c5c7de4ddbcfa27b689a6c424
Carlos Silva <ce4985028@gmail.com>
2026-05-18 23:47:05 -0300

    Criação do projeto do painel administrativo

---
+++ ChapOnibus-admin/src/main/java/br/com/mycompany/chaponibus/admin/ChapOnibusAdmin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.mycompany.chaponibus.admin;
+
+/**
+ *
+ * @author ce498
+ */
+public class ChapOnibusAdmin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
